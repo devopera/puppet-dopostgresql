bfe7428e269f1cd7866152dd648f584980d662f0
cleverlight <github.account@cleveraspaint.com>
2014-12-10 21:37:13 +0000

    minor addition to support and default to postgresql-9.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,18 @@ class dopostgresql (
   # ---------------
   # setup defaults
   
+  $version = '9.2',
+
   # end of class arguments
   # ----------------------
   # begin class
 
 ) {
 
+  class { 'postgresql::globals':
+    manage_package_repo => true,
+    version             => $version,
+  }->
   class { 'postgresql::server':
   }
 
